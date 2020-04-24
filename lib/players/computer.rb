a9392faaded615f3df945dfab6ebecf17cc2d240
Joseph Kao <josephtkao@gmail.com>
2020-04-24 17:56:10 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,6 +3,7 @@ module Players
   class Computer < Player
 
     def move(board)
+      index = nil
       valid_moves = []
       counter = 1
       while counter < 10
@@ -14,11 +15,14 @@ module Players
 
       if valid_moves.include?(5)
         board.update(5, self)
-        index = 5.to_s
-      else
-        index = valid_moves.sample
+
+      elsif board.turn_count == 1
+        index = [1,3,7,9].sample
         board.update(index, self)
-        index.to_s
+      elsif board.turn_count == 3
+        if board.cells[0] == board.cells[8] || board.cells[3] == board.cells[6]
+          index =
+
       end
 
 
